18ceec991ceb59e034abc80cd83a1b2575d29395
inryceu <pavel.maluev@gmail.com>
2026-03-24 22:02:38 +0200

    fixed docs

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -14,7 +14,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import type { Response } from 'express';
 
-import { LoginDto, GoogleMobileLoginDto } from './dto/login-employee.dto';
+import { LoginDto } from './dto/login-employee.dto';
 import { RegisterEmployeeDto } from './dto/register-employee.dto';
 import { RegisterCompanyDto } from './dto/register-company.dto';
 import { LoginCompanyDto } from './dto/login-company.dto';
